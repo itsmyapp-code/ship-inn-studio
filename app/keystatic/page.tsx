51cfbd7c9d8f6cef33bd2821a95e322e2d214809
Martin Cozens <itsmyapp-code@users.noreply.github.com>
2025-12-07 13:43:26 +0000

    Fix: correct keystatic.config path and dynamically import Studio

--- app/keystatic/page.tsx
+++ app/keystatic/page.tsx
@@ -1,6 +1,8 @@
 "use client"
-import { Studio } from '@keystatic/next'
-import config from '../../../keystatic.config'
+import dynamic from 'next/dynamic'
+import config from '../../keystatic.config'
+
+const Studio = dynamic(() => import('@keystatic/next').then((m) => m.Studio), { ssr: false })
 
 export default function KeystaticPage() {
   return <Studio config={config} />
