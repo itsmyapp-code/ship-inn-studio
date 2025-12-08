9ac5a5a513fcc4a538b9de5fc55125f406d010d7
Martin Cozens <itsmyapp-code@users.noreply.github.com>
2025-12-08 06:57:05 +0000

    Refactor: remove all Sanity CMS dependencies and files

--- src/app/api/keystatic/[...params]/route.ts
+++ src/app/api/keystatic/[...params]/route.ts
@@ -1,21 +1,7 @@
 import { makeRouteHandler } from '@keystatic/next/route-handler'
 import config from '../../../../../keystatic.config'
 
-export const dynamic = 'force-dynamic'
-
-const { GET: _GET, POST: _POST } = makeRouteHandler({
+export const { GET, POST } = makeRouteHandler({
   config,
   secret: process.env.KEYSTATIC_SECRET || 'fallback-secret-key-for-ship-inn-studio-123',
 })
-
-export async function GET(request: Request, props: { params: Promise<{ params: string[] }> }) {
-  const params = await props.params
-  // @ts-ignore
-  return _GET(request, { params })
-}
-
-export async function POST(request: Request, props: { params: Promise<{ params: string[] }> }) {
-  const params = await props.params
-  // @ts-ignore
-  return _POST(request, { params })
-}
