38dc8717644b4e8323e8b931ca902c9e4ec99c0b
Martin Cozens <itsmyapp-code@users.noreply.github.com>
2025-12-08 07:18:55 +0000

    Fix: Remove Sanity Studio folder and related files to resolve build error

--- studio/sanity.cli.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  api: {
-    // Replace the projectId below if yours is different
-    projectId: '98yj74tk',
-    dataset: 'production',
-  },
-}
--- studio/schemas/index.ts
+++
@@ -1,4 +0,0 @@
-import news from './news'
-import event from './event'
-
-export default [news, event]
