734826b907c4a711211e4464aea493c826324a0c
Martin Cozens <itsmyapp-code@users.noreply.github.com>
2025-12-13 09:46:05 +0000

    Fix: Update hardcoded Client Secret with fresh value

--- src/app/api/keystatic/[...params]/route.ts
+++ src/app/api/keystatic/[...params]/route.ts
@@ -4,7 +4,7 @@ import config from '../../../../../keystatic.config'
 // HARDCODED FALLBACK FOR DEBUGGING ONLY
 const FALLBACK_SECRET = '3f664d8ac4622c86018c9ff4158d81f4e2b9a0c7d3e5f1g6h8i9j0k1l2m3n4o'
 const FALLBACK_CLIENT_ID = 'Ov23liJWu8geK8HsiyH0'
-const FALLBACK_CLIENT_SECRET = 'ccb835f1ad02ca88ca80577a8b9089b02351a298'
+const FALLBACK_CLIENT_SECRET = '01a1223c25936d38e8c03dcbd67bdb4477596dfc'
 
 export const { GET, POST } = makeRouteHandler({
   config,
